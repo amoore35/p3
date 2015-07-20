3dcce10bf4e623db2b31c69400e08300e3298c0b
aimoore <aimoore@ncsu.edu>
2015-07-20 19:21:00 -0400

    Just testing something

--- Project3/src/edu/ncsu/csc216/book_quiz/question/BookQuestions.java
+++ Project3/src/edu/ncsu/csc216/book_quiz/question/BookQuestions.java
@@ -14,7 +14,7 @@ import edu.ncsu.csc216.question_library.StandardQuestion;
 /**
  * Implements a Finite State Machine for question sequencing, additions, and transitions.
  * [SEE UC4 AND UC7]
- * @author AlexiaMoore
+ * @author Alexia Moore!
  *
  */
 public class BookQuestions {
